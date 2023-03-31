67ab52cbc0e3bd1d6024552fd0cfcbbfe03588b4
dmkur <fgameua@gmail.com>
2023-03-31 16:51:11 +0300

    add login axios

--- api/index.js
+++ api/index.js
@@ -6,8 +6,17 @@ const app = express();
 
 const {usersRoute, authRoute, commentsRoute, likesRoute, postsRoute} = require('./routes')
 
+// need to work with cookies
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", true)
+
+    next()
+})
+
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:'http://localhost:3000'
+}))
 app.use(cookieParser())
 
 app.get('/ping', (req, res) => {
