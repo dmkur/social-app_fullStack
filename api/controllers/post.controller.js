fa67e8eae2af9ee903490171ff1819e25c4c8a6d
dmkur <fgameua@gmail.com>
2023-04-02 13:48:42 +0300

    add posts by followers

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,12 +1,24 @@
 const {db} = require("../connect");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
-    getPosts: (req, res, next)=>{
-         const q = "SELECT p.*, u.id as userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) ";
+    getPosts: (req, res, next) => {
+        const token = req.cookies.accessToken
+        if (!token) return res.status(401).json('Not logged in')
 
-         db.query(q,(err, data)=>{
-             if(err) return res.status(500).json(err)
-             return res.json(data)
-         })
+        jwt.verify(token, 'secretKey', (err, userInfo) => {
+            // оск, при ств токену ми викор userID ми отрим. йоно з useerInfo.id
+            if (err) return res.status(403).json('Token is`t valid')
+
+            // перша кверя підтягує пости + нашого юзера, друга обирає лише пости юзерів на які ми підписані
+            const q = "SELECT p.*, u.id as userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) LEFT JOIN relationships as r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId=? ORDER BY createdAt DESC ";
+
+            db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+                if (err) return res.status(500).json(err)
+                return res.json(data)
+            })
+        })
+
+
     }
 }
